a5e1f24ee2b407e57d72ed7beb15b0c692df6a51
Anton-by <61452174+Anton-by@users.noreply.github.com>
2020-10-03 14:21:48 +0300

    feat: implement 3 functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-
-exports.min = function min (array) {
+exports.min = function min(array) {
+  if (array !== undefined && array.length) {
+      let minElem = array[0];
+      for (let i = 0; i <= array.length - 1; i++) {
+          if (array[i] < minElem) {
+              minElem = array[i];
+          }
+      }
+      return minElem;
+  }
   return 0;
-}
+};
 
-exports.max = function max (array) {
+exports.max = function max(array) {
+  if (array !== undefined && array.length) {
+      let maxElem = array[0];
+      for (let i = 0; i <= array.length - 1; i++) {
+          if (array[i] > maxElem) {
+              maxElem = array[i];
+          }
+      }
+      return maxElem;
+  }
   return 0;
-}
+};
 
-exports.avg = function avg (array) {
+exports.avg = function avg(array) {
+  if (array !== undefined && array.length) {
+      let sumArray = 0;
+      let arrayItemsCount = array.length;
+      for (let i = 0; i <= array.length - 1; i++) {
+          sumArray += array[i];
+      }
+      return sumArray / arrayItemsCount;
+  }
   return 0;
-}
+};
